59c43209435593822dd9e23ec6cf33df49bc267d
Angeltrst03 <114330696+Angeltrst03@users.noreply.github.com>
2022-11-29 19:03:49 -0600

    Add files via upload

---
+++ 3 Scripting en Bash/bro.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# 27/Septiembre/2022  
+#Jorge Angel Valdez Tristan 1957496 
+
+read -p "Escribe tu nombre: " nombre
+echo "Hola, $nombre. Seamos amigos!"
---
+++ 3 Scripting en Bash/welcome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 27 septiembre 2022
+# Jorge Angel Valdez TRISTAN 
+#
+echo "Hola ${LOGNAME}"
+echo "Hoy es $(date)"
+echo "Usuarios actuales conectados, y sus procesos:"amigos!"
